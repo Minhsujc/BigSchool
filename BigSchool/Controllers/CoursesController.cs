e7e5fe5557a56b20fa9680edc8baac253119b1c3
Minhsujc <minhthanh06052000@gmail.com>
2021-07-08 22:01:49 +0700

    BigSchool

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,51 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            //get List Category
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = context.Categories.ToList();
+
+            return View(objCourse);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+           
+            //Khong xet Valid LectureId vi bang user dang nhap
+            ModelState.Remove("LecturerId");
+            if(!ModelState.IsValid)
+            {
+                objCourse.ListCategory = context.Categories.ToList();
+                return View("Create", objCourse);
+            }
+
+            //lay login user Id
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            //add vào CSDL
+            context.Courses.Add(objCourse);
+            context.SaveChanges();
+
+            //Trở về home
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ BigSchool/Models/BigSchoolContext.cs
@@ -0,0 +1,29 @@
+using BigSchool.Controllers;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace BigSchool.Models
+{
+    public partial class BigSchoolContext : DbContext
+    {
+        public BigSchoolContext()
+            : base("name=BigSchoolContext")
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+
+        public virtual DbSet<Attendance> Attendances { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasMany(e => e.Courses)
+                .WithRequired(e => e.Category)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
